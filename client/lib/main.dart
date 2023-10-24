b6940b0c8503f4b4ac83b5b7e52455b83de05e4b
Annie Tran <ngoc.tran@hope.edu>
2023-10-23 20:26:58 -0400

    Added package and the homepage

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -9,12 +9,60 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      theme: ThemeData.light().copyWith(
+        scaffoldBackgroundColor: const Color.fromARGB(255, 59, 132, 210),
       ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.light().copyWith(
+        scaffoldBackgroundColor: const Color.fromARGB(255, 59, 132, 210),
+      ),
+      home: Scaffold(
+          body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Container(
+                width: 133,
+                height: 133,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(
+                        "https://static.thenounproject.com/png/3972603-200.png"),
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(20.0),
+              child: Text('WalkieTalkie',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 30,
+                    fontFamily: 'Alata',
+                    fontWeight: FontWeight.w400,
+                    height: 0.04,
+                    letterSpacing: 1.05,
+                  )),
+            )
+          ],
+        ),
+      )),
     );
   }
 }
